7209c729d5de46c63bbf6ea0066bfd81090e7cc8
codev911 <codev911@gmail.com>
2024-12-23 14:50:54 +0700

    update deploy

--- docker/setup_env.sh
+++ docker/setup_env.sh
@@ -7,6 +7,6 @@ echo "Generating .env file for ya!"
 cp -n ./.env_template ./.env
 sed -i -e "s|SECRET_KEY_BASE=<genearate a secure key>|SECRET_KEY_BASE=$(openssl rand -base64 48)|g" ./.env
 sed -i -e "s|NODE_COOKIE=<genearate another secure key>|NODE_COOKIE=$(openssl rand -base64 48)|g" ./.env
-rm ./.env-e
+# rm ./.env-e # need to be disable because no file name .env-e
 
 echo "Done :)"
